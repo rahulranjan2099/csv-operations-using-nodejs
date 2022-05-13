874e17b30dcb7c950b30c4ce8a3e60f583b8cc2e
rahulranjan2099 <103832877+rahulranjan2099@users.noreply.github.com>
2022-05-13 13:06:57 +0530

    ready

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/',(req,res)=>{
     res.redirect('/')
 })
 
-app.listen(3000,()=>{
-    console.log("PORT STARTING AT 3000")
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`PORT STARTING AT ${port}`)
 })
